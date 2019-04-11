2e81f41b65792859db17cf8d8689adf8b270e955
DanManN <dnahimov@gmail.com>
2019-04-10 20:55:08 -0400

    ringmod

--- hw4.m
+++ hw4.m
@@ -28,18 +28,18 @@ STDERR=2;                               % Define the standard error stream
 %% Sound Samples
 % claims to be guitar
 % source: http://www.traditionalmusic.co.uk/scales/musical-scales.htm
-[guitarSound, fsg] = wavread('guitar_C_major.wav');
+[pianoSound, fsg] = audioread('piano_C_major.wav');
 
 % sax riff - should be good for compressor
 % source: http://www.freesound.org/people/simondsouza/sounds/763
-[saxSound, fss] = wavread('sax_riff.wav');
+[saxSound, fss] = audioread('sax_riff.wav');
 
 % a fairly clean guitar riff
 % http://www.freesound.org/people/ERH/sounds/69949/
-[cleanGuitarSound, fsag] = wavread('guitar_riff_acoustic.wav');
+[cleanGuitarSound, fsag] = audioread('guitar_riff_acoustic.wav');
 
 % Harmony central drums (just use the first half)
-[drumSound, fsd] = wavread('drums.wav');
+[drumSound, fsd] = audioread('drums.wav');
 L = size(drumSound,1);
 drumSound = drumSound(1:round(L/2), :);
 
@@ -57,20 +57,21 @@ LFO_rate=0.5;
 threshold = 0.1; 
 attack = 0.2;
 avg_len = 5000;
+slope = 0.5;
 [output,gain]=compressor(constants,saxSound,threshold,slope,avg_len);
 
 soundsc(saxSound,constants.fs)
 disp('Playing the Compressor input')
 soundsc(output,constants.fs)
 disp('Playing the Compressor Output');
-wavwrite(output,fss,'output_compressor.wav');
+%audiowrite(output,fss,'output_compressor.wav');
 
 % PLOTS for Question 1d
 
 % TODO: Add code to complete plots
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Question 2 - Ring Modulator
+%% Question 2 - Ring Modulator
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 constants.fs = fsg;
 % the input frequency is fairly arbitrary, but should be about an order of
@@ -78,17 +79,18 @@ constants.fs = fsg;
 % consistent-sounding effect
 inputFreq = 2500;
 depth = 0.5;
-[output]=ringmod(constants,guitarSound,inputFreq,depth);
+[output]=ringmod(constants,pianoSound,inputFreq,depth);
 
-soundsc(guitarSound,constants.fs)
+soundsc(pianoSound,constants.fs)
 disp('Playing the RingMod input')
+pause
 soundsc(output,constants.fs)
 disp('Playing the RingMod Output');
-wavwrite(output,fsg,'output_ringmod.wav');
+%audiowrite(output,fsg,'output_ringmod.wav');
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Question 3 - Stereo Tremolo
+%% Question 3 - Stereo Tremolo
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 LFO_type = 'sin';
 LFO_rate = 5;
@@ -96,13 +98,13 @@ LFO_rate = 5;
 % the lag becomes very noticeable one the difference is about 1/10 sec
 lag = constants.fs/4;
 depth = 0.9;
-[output]=tremolo(constants,guitarSound,LFO_type,LFO_rate,lag,depth);
+[output]=tremolo(constants,pianoSound,LFO_type,LFO_rate,lag,depth);
 
-soundsc(guitarSound,constants.fs)
+soundsc(pianoSound,constants.fs)
 disp('Playing the Tremolo input')
 soundsc(output,constants.fs)
 disp('Playing the Tremolo Output');
-wavwrite(output,fsg,'output_tremelo.wav');
+%audiowrite(output,fsg,'output_tremelo.wav');
 
 
 
@@ -118,7 +120,7 @@ soundsc(inSound,constants.fs)
 disp('Playing the Distortion input')
 soundsc(output,constants.fs)
 disp('Playing the Distortion Output');
-wavwrite(output,fsag,'output_distortion.wav');
+%audiowrite(output,fsag,'output_distortion.wav');
 
 % look at what distortion does to the spectrum
 L = 10000;
@@ -129,7 +131,7 @@ sinSound = sin(2*pi*440*(n/fsag));
 % TODO: Add some Sample code to demonstrate the spectrum 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Question 5 - Delay
+%% Question 5 - Delay
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % slapback settings
@@ -143,11 +145,11 @@ soundsc(inSound,constants.fs)
 disp('Playing the Slapback input')
 soundsc(output,constants.fs)
 disp('Playing the Slapback Output');
-wavwrite(output,fsag,'output_slapback.wav');
+%audiowrite(output,fsag,'output_slapback.wav');
 
 
 % cavern echo settings
-inSound = guitarSound;
+inSound = pianoSound;
 delay_time = 0.4;
 depth = 0.8;
 feedback = 0.7;
@@ -157,11 +159,11 @@ soundsc(inSound,constants.fs)
 disp('Playing the cavern input')
 soundsc(output,constants.fs)
 disp('Playing the cavern Output');
-wavwrite(output,fsh,'output_cave.wav');
+%audiowrite(output,fsh,'output_cave.wav');
 
 
 % delay (to the beat) settings
-inSound = guitarSound;
+inSound = pianoSound;
 delay_time = 0.18;
 depth = 1;
 feedback = 1;
@@ -171,10 +173,10 @@ soundsc(inSound,constants.fs)
 disp('Playing the delayed on the beat input')
 soundsc(output,constants.fs)
 disp('Playing the delayed on the beat Output');
-wavwrite(output,fsg,'output_beatdelay.wav');
+%audiowrite(output,fsg,'output_beatdelay.wav');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Question 6 - Flanger
+%% Question 6 - Flanger
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 inSound = drumSound;
 constants.fs = fsd;
@@ -188,14 +190,14 @@ soundsc(inSound,constants.fs)
 disp('Playing the Flanger input')
 soundsc(output,constants.fs)
 disp('Playing the Flanger Output');
-wavwrite(output,fsd,'output_flanger.wav');
+%audiowrite(output,fsd,'output_flanger.wav');
 
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Question 7 - Chorus
+%% Question 7 - Chorus
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-inSound = guitarSound(:,1);
+inSound = pianoSound(:,1);
 constants.fs = fsg;
 depth = 0.9;
 delay = .03;   
@@ -207,6 +209,6 @@ soundsc(inSound,constants.fs)
 disp('Playing the Chorus input')
 soundsc(output,constants.fs)
 disp('Playing the Chorus Output');
-wavwrite(output,fsg,'output_chorus.wav');
+%audiowrite(output,fsg,'output_chorus.wav');
 
 
--- ringmod.m
+++ ringmod.m
@@ -1,2 +1,4 @@
 function [output] = ringmod(constants,inSound,inputFreq,depth)
 %RINGMOD applies ring modulator effect to inSound
+assert(0 <= depth && depth <= 1)
+output = (depth * sin(2*pi*inputFreq*(1:length(inSound))/constants.fs)'.*inSound + inSound)/(1+depth);
